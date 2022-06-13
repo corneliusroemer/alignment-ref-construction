dc72aee9864897e9c1c58c552d84c8d8ba043c1c
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-06-13 17:33:29 +0200

    Add script args through typer

--- Snakefile
+++ Snakefile
@@ -21,38 +21,16 @@ rule tree:
             --output {output}
         """
 
-rule infer_ancestors:
-    input: 
-        tree = rules.tree.output,
-        alignment = rules.align_fastas.output
-    output: "build/ancestral_reconstruction/ancestral_sequences.fasta"
-    shell: """
-        treetime ancestral \
-            --aln {input.alignment}\
-            --tree {input.tree} \
-            --keep-overhangs \
-            --outdir build/ancestral_reconstruction
-        """
-
-rule tree_with_ancestors:
-    input: rules.infer_ancestors.output
-    output: "build/tree_with_ancestors.nwk"
-    shell: """
-        augur tree \
-            --alignment {input} \
-            --output {output}
-        """
-
 rule pick_ancestral_sequence:
     input:
-        tree = rules.tree_with_ancestors.output,
+        tree = rules.tree.output,
+        alignment = rules.align_fastas.output,
         mpx_sequences = "input/mpx/mpx_assortment.fasta",
-        ancestral = "build/ancestral_reconstruction/ancestral_sequences.fasta"
     output: "output/ref_to_coord.fasta"
     shell: """
         python pick_ancestral_sequence.py \
-            --tree {input.tree} \
-            --mpx_sequences {input.mpx_sequences} \
-            --ancestral {input.ancestral} \
-            --output {output}
+            {input.tree} \
+            {input.alignment} \
+            {input.mpx_sequences} \
+            {output}
         """
--- pick_ancestral_sequence.py
+++ pick_ancestral_sequence.py
@@ -1,46 +1,41 @@
 #%%
-from Bio import Phylo, Seq, SeqIO
-from treetime import TreeAnc
+import typer
 
 #%%
-tree = Phylo.read("build/tree.nwk", "newick")
-tree.root_with_outgroup("NC_003663.2","KY369926.1")
-# Phylo.draw(tree)
-#%%
-tt = TreeAnc(tree, "build/aligned.fasta", fill_overhangs=False)
-# %%
-tree_dict = tt.get_tree_dict()
-#%%
-td = {}
-for sequence in tree_dict:
-    td[sequence.id] = sequence.seq
-td
-#%%
-mpx_sequences = SeqIO.to_dict(SeqIO.parse("input/mpx/mpx_assortment.fasta", "fasta"))
-# %%
-mpx_terminals = []
-for terminal in tree.get_terminals():
-    if terminal.name in mpx_sequences.keys():
-        mpx_terminals.append(terminal)
+def main(tree_path: str, alignment: str, mpx_sequences: str, output: str):
+    from Bio import Phylo, Seq, SeqIO
+    from treetime import TreeAnc
+    tree = Phylo.read(tree_path, "newick")
+    tree.root_with_outgroup("NC_003663.2","KY369926.1")
 
+    tt = TreeAnc(tree, alignment, fill_overhangs=False)
 
-ancestor_name = tt.tree.common_ancestor(mpx_terminals)
+    tree_dict = tt.get_tree_dict()
+    td = {}
+    for sequence in tree_dict:
+        td[sequence.id] = sequence.seq
 
-#%%
-anc = str(td['NODE_0000004'])
-coord = str(td['NC_063383'])
+    mpx_sequences = SeqIO.to_dict(SeqIO.parse(mpx_sequences, "fasta"))
+    mpx_terminals = []
+    for terminal in tree.get_terminals():
+        if terminal.name in mpx_sequences.keys():
+            mpx_terminals.append(terminal)
 
-# %%
-output_list = []
-for (ref,coord) in zip(anc,coord):
-    if coord != "-":
-        if ref == "-":
-            output_list.append(coord)
-        else:
-            output_list.append(ref)
-# %%
-output_seq = "".join(output_list)
-# %%
-SeqIO.write(SeqIO.SeqRecord(Seq.Seq(output_seq), id="ref_in_coord", description="Reference sequence in coord.fasta coordinates"), "output/ref_to_coord.fasta", "fasta")
+    ancestor_name = tt.tree.common_ancestor(mpx_terminals).name
 
-# %%
+    anc = str(td[ancestor_name])
+    coord = str(td['NC_063383'])
+
+    output_list = []
+    for (ref,coord) in zip(anc,coord):
+        if coord != "-":
+            if ref == "-":
+                output_list.append(coord)
+            else:
+                output_list.append(ref)
+    output_seq = "".join(output_list)
+    # %%
+    SeqIO.write(SeqIO.SeqRecord(Seq.Seq(output_seq), id="ref_in_coord", description="Reference sequence in coord.fasta coordinates"), output, "fasta")
+
+if __name__ == "__main__":
+    typer.run(main)
